a7de3ec4baecfe629d028c304db09b95f7b94d97
zhunzhun <2771542263@qq.com>
2026-05-29 13:51:32 +0800

    chore: 重构项目品牌标识为通用Admin
    1. 移除文件头部版权注释、修改页面标题为通用"管理系统"
    2. 替换底部版权文本、包信息和项目配置前缀
    3. 移除内置的源码地址和文档地址链接
    4. 清理导航栏自带的官方链接组件

--- ruoyi-ui/src/layout/components/Navbar.vue
+++ ruoyi-ui/src/layout/components/Navbar.vue
@@ -12,13 +12,7 @@
       <template v-if="device!=='mobile'">
         <search id="header-search" class="right-menu-item" />
 
-        <el-tooltip content="源码地址" effect="dark" placement="bottom">
-          <ruo-yi-git id="ruoyi-git" class="right-menu-item hover-effect" />
-        </el-tooltip>
-
-        <el-tooltip content="文档地址" effect="dark" placement="bottom">
-          <ruo-yi-doc id="ruoyi-doc" class="right-menu-item hover-effect" />
-        </el-tooltip>
+        
 
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
@@ -66,8 +60,7 @@ import Hamburger from '@/components/Hamburger'
 import Screenfull from '@/components/Screenfull'
 import SizeSelect from '@/components/SizeSelect'
 import Search from '@/components/HeaderSearch'
-import RuoYiGit from '@/components/RuoYi/Git'
-import RuoYiDoc from '@/components/RuoYi/Doc'
+
 import HeaderNotice from './HeaderNotice'
 
 export default {
@@ -80,8 +73,6 @@ export default {
     Screenfull,
     SizeSelect,
     Search,
-    RuoYiGit,
-    RuoYiDoc,
     HeaderNotice
   },
   computed: {
